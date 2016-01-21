396328c9a97e1d193355d68c698e61c481c54f1c
moderndaymoron <ragnaringir@gmail.com>
2016-01-21 15:00:47 +0000

    line temp canvas

--- js/line.js
+++ js/line.js
@@ -7,8 +7,18 @@ class Line extends Shape {
 	draw(ctx){
 		console.log("drawing line");
 		var bounds = this.calcBounds();
+		//ctx.beginPath();
 		ctx.moveTo(this.x, this.y);
 		ctx.lineTo(this.endX, this.endY);            
 		ctx.stroke();
+		//ctx.endPath();
+	}
+	move(ctx, e){
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+    	ctx.beginPath();
+    	ctx.moveTo(this.x, this.y);
+    	ctx.lineTo(e.offsetX, e.offsetY);
+    	ctx.stroke();
+    	ctx.closePath();
 	}
 }
