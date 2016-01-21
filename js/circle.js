9c3c09b7098124fd431f752a3b90d81a8b098c16
Árni Oddsson <arniodds@gmail.com>
2016-01-21 15:02:01 +0000

    Circle tool working

--- js/circle.js
+++ js/circle.js
@@ -6,6 +6,22 @@ class Circle extends Shape {
 
 	draw(ctx){
 		var bounds = this.calcBounds();
-		ctx.strokeRect(bounds.x, bounds.y, bounds.maxX, bounds.maxY);
+		//ctx.strokeRect(bounds.x, bounds.y, bounds.maxX, bounds.maxY);
 	}
-}
+
+	move(ctx, e){
+	    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+		var x = (e.offsetX + this.x) / 2;
+    	var y = (e.offsetY + this.y) / 2;
+ 
+    	var radius = Math.max(
+        	Math.abs(this.x - e.offsetX),
+        	Math.abs(this.y - e.offsetY)) / 2;
+ 
+    	ctx.beginPath();
+    	ctx.arc(x, y, radius, 0, Math.PI*2, false);
+    	ctx.stroke();
+    	ctx.closePath();
+	}
+}	
